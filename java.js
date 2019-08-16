1c75762facd16c18cba8efe14fd85db971e993b9
rehamhisham <53395570+rehamhisham@users.noreply.github.com>
2019-08-16 18:21:41 +0300

    3456

---
+++ java.js
@@ -0,0 +1,15 @@
+/*var x = "hello"
+var massage = "in globel";
+console.log("globel: message = " + message);
+
+var a = function(){
+    var message = "inside a";
+    console.log("a: message = " + message)
+   
+    var b = function(){
+        var message = "inside a";
+        console.log("b: message = " + message)}
+        b();
+}
+
+    a();*/
